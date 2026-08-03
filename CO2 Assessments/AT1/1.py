da4150e1cd182c7a348d053e81771db339a7b385
has211 <govindhasini571@gmail.com>
2026-08-03 08:29:34 +0530

    Create 1.py

---
+++ CO2 Assessments/AT1/1.py
@@ -0,0 +1,29 @@
+words = ["connected", "connecting", "connection"]
+
+for word in words:
+    if word.endswith("ed"):
+        root = word[:-2]
+        suffix = "ed"
+        t = "Inflectional"
+
+    elif word.endswith("ing"):
+        root = word[:-3]
+        suffix = "ing"
+        t = "Inflectional"
+
+    elif word.endswith("ion"):
+        root = "connect"
+        suffix = "ion"
+        t = "Derivational"
+
+    else:
+        root = word
+        suffix = "-"
+        t = "-"
+
+    print("Word :", word)
+    print("Root :", root)
+    print("Suffix :", suffix)
+    print("Type :", t)
+    print("Normalized :", root)
+    print()
